b88aaa98983ea01047b1ab19b19cd2f7ce6f1878
Victor Michaux <victor.michaux@gmail.com>
2018-04-08 22:10:02 +0200

    Launchscreen

---
+++ app/src/main/java/efrei/edu/projetandroid/HomeActivity.java
@@ -0,0 +1,15 @@
+package efrei.edu.projetandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
