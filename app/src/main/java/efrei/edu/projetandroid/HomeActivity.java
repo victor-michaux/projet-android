de65f672302da47a690b0538dce48eb08d2d8554
Victor Michaux <victor.michaux@gmail.com>
2018-05-03 10:04:12 +0200

    Old games list view template (#1)

--- app/src/main/java/efrei/edu/projetandroid/HomeActivity.java
+++ app/src/main/java/efrei/edu/projetandroid/HomeActivity.java
@@ -1,7 +1,9 @@
 package efrei.edu.projetandroid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
@@ -11,5 +13,11 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void onOldGamesClick(View view)
+    {
+        Intent oldGameIntent = new Intent(this, OldGameActivity.class);
+        startActivity(oldGameIntent);
     }
 }
